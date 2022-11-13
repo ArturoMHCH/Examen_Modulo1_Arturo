8056aa6f20c41b89745e94000b9ce308ea5730c9
ArturoMHCH <mscaljrokline@gmail.com>
2022-11-12 19:08:55 -0600

    Creando la clase figurageometrica

---
+++ Final_Modulo1_Arturo/FiguraGeometrica.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Modulo1_Arturo
+{
+    public class FiguraGeometrica
+    {
+        public string nombre { get; set; }
+    }
+}
