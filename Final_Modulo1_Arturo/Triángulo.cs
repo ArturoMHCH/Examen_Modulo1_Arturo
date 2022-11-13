bb5b841503a6a2ea0ce5ec2be6f22114de2f6941
ArturoMHCH <mscaljrokline@gmail.com>
2022-11-12 20:43:56 -0600

    Aumentando atributos y metodo tostring en clase triangulo

--- Final_Modulo1_Arturo/Triángulo.cs
+++ Final_Modulo1_Arturo/Triángulo.cs
@@ -6,9 +6,17 @@ namespace Final_Modulo1_Arturo
 {
     public class Triángulo : FiguraGeometrica, Poligono
     {
+        public double ladoizq { get; set; }
+        public double ladoder { get; set; }
+        public double ladobase { get; set; }
+        public double altura { get; set; }
         public double area(double valor1, double valor2)
         {
             return ((valor1*valor2) / 2);
         }
-    }
+        public string ToString()
+        {
+            return (nombre + " "+area(ladobase, altura).ToString());
+        }
+}
 }
