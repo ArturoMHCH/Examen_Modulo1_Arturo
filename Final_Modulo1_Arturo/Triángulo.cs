2c5f28c400d2a8d356e43cb36080f78b185a2f50
ArturoMHCH <mscaljrokline@gmail.com>
2022-11-12 19:21:00 -0600

    Creando clase triangulo

---
+++ Final_Modulo1_Arturo/Triángulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Modulo1_Arturo
+{
+    public class Triángulo : FiguraGeometrica, Poligono
+    {
+        public double area(double valor1, double valor2)
+        {
+            return ((valor1*valor2) / 2);
+        }
+    }
+}
