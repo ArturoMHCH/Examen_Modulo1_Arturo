fec264692be5de00a011f3c469be73ba6eb5f999
ArturoMHCH <mscaljrokline@gmail.com>
2022-11-13 09:01:38 -0600

    Creando metodo area y tostring, ademas de heredar y poner interface

--- Final_Modulo1_Arturo/Rectangulo.cs
+++ Final_Modulo1_Arturo/Rectangulo.cs
@@ -4,9 +4,18 @@ using System.Text;
 
 namespace Final_Modulo1_Arturo
 {
-    public class Rectangulo
+    public class Rectangulo : FiguraGeometrica, Poligono
     {
         public double ladobase { get; set; }
         public double ladoalto { get;set; }
+
+        public double area(double valor1, double valor2)
+        {
+            return (valor1 * valor2);
+        }
+        public string ToString()
+        {
+            return (nombre + " " + area(ladobase, ladoalto).ToString());
+        }
     }
 }
