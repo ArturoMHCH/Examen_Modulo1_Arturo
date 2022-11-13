aba81f8968198156c3aa654a8572786021b727a7
ArturoMHCH <mscaljrokline@gmail.com>
2022-11-13 08:59:01 -0600

    Creando clase rectangulo con sus atributos

---
+++ Final_Modulo1_Arturo/Rectangulo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Modulo1_Arturo
+{
+    public class Rectangulo
+    {
+        public double ladobase { get; set; }
+        public double ladoalto { get;set; }
+    }
+}
