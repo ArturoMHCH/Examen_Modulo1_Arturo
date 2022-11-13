f8efe5e8090d06def867211f6eb839cf29844d28
ArturoMHCH <mscaljrokline@gmail.com>
2022-11-12 19:11:07 -0600

    Creando la interfaz poligono

---
+++ Final_Modulo1_Arturo/Poligono.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Modulo1_Arturo
+{
+    public interface Poligono
+    {
+        public double area(double valor1, double valor2);
+    }
+}
