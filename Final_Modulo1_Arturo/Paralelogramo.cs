279a15e5b0f3cef51eb40955b960cf792dcc1571
ArturoMHCH <mscaljrokline@gmail.com>
2022-11-13 09:36:40 -0600

    Creando clase paralelogramo con sus atributos, tambien comentarios de datos

---
+++ Final_Modulo1_Arturo/Paralelogramo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Modulo1_Arturo
+{
+    public class Paralelogramo
+    {
+        /*
+        Paralelogramo:
+
+
+           .----------
+          .         .
+         .         .ladoinclinado
+        .angulo   .
+        ----------
+          ladobase
+         */
+        public double ladobase { get; set; }
+        public double ladoinclinado { get; set; }
+        public int angulo { get; set; }
+    }
+}
